067d90ff73b7456fbae00d662cb228d157658b0b
Dolly4284 <djean.pulgarinas@gmail.com>
2024-08-10 03:28:52 +0000

    Create table mySQL

---
+++ public_html/database01.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . mysqli_error($conn);
+}
+
+?>
+
