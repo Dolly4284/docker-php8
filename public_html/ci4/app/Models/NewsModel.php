41283c847cf2e3f6dfd27cf7be8223b86d846aef
Dolly4284 <djean.pulgarinas@gmail.com>
2024-08-17 00:44:56 +0000

    CI4 Static Pages and News Turotial

--- public_html/ci4/app/Models/NewsModel.php
+++ public_html/ci4/app/Models/NewsModel.php
@@ -8,6 +8,8 @@ class NewsModel extends Model
 {
     protected $table = 'news';
 
+    protected $allowedFields = ['title', 'slug', 'body'];
+
     public function getNews($slug = false)
     {
         if ($slug === false) {
@@ -15,5 +17,5 @@ class NewsModel extends Model
         }
 
         return $this->where(['slug' => $slug])->first();
-    }
+    }   
 }
